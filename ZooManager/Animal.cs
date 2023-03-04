fc17d936ae1db667c24b9f73d774ffc589450d14
MaskMan-DU <duyiran19385@gmail.com>
2023-03-04 01:39:47 -0500

    Fix Bug

--- ZooManager/Animal.cs
+++ ZooManager/Animal.cs
@@ -43,7 +43,7 @@ namespace ZooManager
             return isActivated = true;
         }
 
-        // Improve Feature: ()
+        // Improve Feature: (g) Wirte a new method Move that use 3 pieces of information: an Animal, a Direction, and a Distance. The specified animal will move in the specified direction 1 square at a time, but will only into an empty square. If the next square in that direction is not empty, the animal will not move.
         /// <summary>
         /// Let the runner move in a random direction other than hunterDirecton, and the maximum number of steps to move is determined by Distance.
         /// </summary>
@@ -53,10 +53,12 @@ namespace ZooManager
         /// Is called by Mouse object
         protected virtual int Move(Animal runner, Direction hunterDirecton, int Distance)
         {
-            List<Direction> allDirection = new List<Direction> { Direction.down, Direction.up, Direction.left, Direction.right};
-            allDirection.Remove(hunterDirecton);
+            // The following code is to disrupt the direction list after removing the hunterDirection.
+            List<Direction> allDirection = new List<Direction> { Direction.down, Direction.up, Direction.left, Direction.right}; // Add a new list which contain 4 direction
+            allDirection.Remove(hunterDirecton); // Remove th hunt direction in the list
             Random random = new Random();
-            List<Direction> moveDirection = new List<Direction>();
+            List<Direction> moveDirection = new List<Direction>(); // Build a new list to store posible move direction
+            // Disrupt the moveDirection list
             foreach (Direction direction in allDirection)
             {
                 moveDirection.Insert(random.Next(moveDirection.Count + 1), direction);
@@ -65,40 +67,50 @@ namespace ZooManager
             int x = runner.location.x;
             int y = runner.location.y;
 
-            int distance = 0;
-            bool canMove = false;
+            int distance = 0; // the vairable to record the actual move distance 
+            bool canMove = false; // Add a bool value to remember whether the animal can move
 
+            // use for loop to check all direction in the moveDirection list
             for(int i = 0; i < 3; i++)
             {
                 if (moveDirection[i] == Direction.up) // direction is up
                 {
+                    // use for loop to make the animal move the distance given by Distance.
                     for (int d = 0; d < Distance; d++)
                     {
-                        if (y - d > 0 && x >= 0 && x <= Game.numCellsX - 1 && Game.animalZones[y - (d + 1)][x].occupant == null)
+                        if (y - d > 0 && x >= 0 && x <= Game.numCellsX - 1 && Game.animalZones[y - (d + 1)][x].occupant == null) // The animal is not out of range in the x-direction and the coordinate y - d remains within the range in the y-direction, while the zone on (y - d, x) is empty.
                         {
                             Game.animalZones[y - (d + 1)][x].occupant = runner;
                             Game.animalZones[y - d][x].occupant = null;
                             distance++;
                             canMove = true;
                         }
+                        else // When a failure is determined, the loop is directly jumped out to prevent errors. Error example: The first move fails, but the second move succeeds.
+                        {
+                            break;
+                        }
                     }
 
-                    if (canMove)
+                    if (canMove) // Only a successful move will return distance and jump out of the whole loop.
                     {
                         return distance;
                     }
                 }
-                else if (moveDirection[i] == Direction.down)
+                else if (moveDirection[i] == Direction.down) // direction is down
                 {
                     for (int d = 0; d < Distance; d++)
                     {
-                        if (y + d < Game.numCellsY - 1 && x >= 0 && x <= Game.numCellsX - 1 && Game.animalZones[y + (d + 1)][x].occupant == null)
+                        if (y + d < Game.numCellsY - 1 && x >= 0 && x <= Game.numCellsX - 1 && Game.animalZones[y + (d + 1)][x].occupant == null) // The animal is not out of range in the x-direction and the coordinate y + d remains within the range in the y-direction, while the zone on (y + d, x) is empty.
                         {
                             Game.animalZones[y + (d + 1)][x].occupant = runner;
                             Game.animalZones[y + d][x].occupant = null;
                             distance++;
                             canMove = true;
-                        }                     
+                        }
+                        else
+                        {
+                            break;
+                        }
                     }
 
                     if (canMove)
@@ -107,16 +119,20 @@ namespace ZooManager
                     }
                     
                 }
-                else if (moveDirection[i] == Direction.left)
+                else if (moveDirection[i] == Direction.left) // direction is left
                 {
                     for (int d = 0; d < Distance; d++)
                     {
-                        if (x - d > 0 && y >= 0 && y <= Game.numCellsY -1 && Game.animalZones[y][x - (d + 1)].occupant == null)
+                        if (x - d > 0 && y >= 0 && y <= Game.numCellsY -1 && Game.animalZones[y][x - (d + 1)].occupant == null) // The animal is not out of range in the y-direction and the coordinate x - d remains within the range in the x-direction, while the zone on (y, x - d) is empty.
                         {
                             Game.animalZones[y][x - (d + 1)].occupant = runner;
                             Game.animalZones[y][x - d].occupant = null;
                             distance++;
                             canMove = true;
+                        }
+                        else
+                        {
+                            break;
                         }
                     }
 
@@ -130,12 +146,16 @@ namespace ZooManager
                 {
                     for (int d = 0; d < Distance; d++)
                     {
-                        if (x + d < Game.numCellsX - 1 && y >= 0 && y <= Game.numCellsY - 1 && Game.animalZones[y][x + (d + 1)].occupant == null)
+                        if (x + d < Game.numCellsX - 1 && y >= 0 && y <= Game.numCellsY - 1 && Game.animalZones[y][x + (d + 1)].occupant == null) // The animal is not out of range in the y-direction and the coordinate x + d remains within the range in the x-direction, while the zone on (y, x + d) is empty.
                         {
                             Game.animalZones[y][x + (d + 1)].occupant = runner;
                             Game.animalZones[y][x + d].occupant = null;
                             distance++;
                             canMove = true;
+                        }
+                        else
+                        {
+                            break;
                         }
                     }
 
@@ -146,7 +166,7 @@ namespace ZooManager
                 }
             }
 
-            return distance;
+            return distance; // Returns distance if all the directions in moveDirection cannot be moved, and distance is then 0.
         } 
 
 
